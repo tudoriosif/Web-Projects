1e0a0afe755e1069854119429609968769c0ea3c
Iosif Tudor Andrei <tudor_iosif@yahoo.com>
2020-10-23 14:27:08 +0300

    Showing nodes and graph parsed

--- GraphGenerator/main.js
+++ GraphGenerator/main.js
@@ -1,11 +1,26 @@
 const startBtn = document.getElementById("start-button");
+const refresh = document.getElementsByClassName("details-container")[0];
+let conContainer = document.getElementsByClassName("con-container")[0];
+let nodeContainer = document.getElementsByClassName("node-container")[0];
 let nodesName = document.getElementById("nodes");
-let connectionsName = document.getElementById("connections");  
+let connectionsName = document.getElementById("connections");
 
 function refreshValue() {
+    refresh.style.display = "none"; 
+
+    Array.from(
+        document.getElementsByClassName("con-container")[0]
+        .getElementsByTagName("span"))
+        .forEach(el => el.remove());
+        
+    Array.from(
+        document.getElementsByClassName("node-container")[0].getElementsByTagName("span"))
+        .forEach(el => el.remove());
+
     nodesName.value = '';
     connectionsName.value = '';
     startBtn.classList.remove("correct", "wrong");
+
 }
 
 function startGraph() {
@@ -40,10 +55,31 @@ function parseVars(nodesToParse, connecsToParse) {
                 .map(item => {
                     let parse = item.match(parseNReg);
                      return {[parse[0]] : parse[1]};
-                })
-                .reduce((obj, item) => 
-                (obj[Object.keys(item)[0]] = Object.values(item)[0], obj), {});
+                });
+                // .reduce((obj, item) => 
+                // (obj[Object.keys(item)[0]] = Object.values(item)[0], obj), {});
     
     console.log(nodes);
     console.log(conns);
+    showDetails(nodes, conns);
+}
+
+function showDetails(nodes, conns){
+    refresh.style.display = "flex";
+
+    nodes.forEach(item => {
+        let node = document.createElement("span");
+        node.appendChild(document.createTextNode(item));
+        nodeContainer.appendChild(node);
+        return;
+    });
+
+    conns.forEach(item => {
+        let startPoint = Object.keys(item)[0];
+        let endPoint = Object.values(item)[0];
+        let con = document.createElement("span");
+        con.appendChild(document.createTextNode(startPoint + " \-\> " + endPoint));
+        conContainer.appendChild(con);
+        return;
+    })
 }
