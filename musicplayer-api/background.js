8e59f49b82dac097bebd053a4105e3a29da51aaa
Iosif Tudor Andrei <tudor_iosif@yahoo.com>
2020-11-26 07:33:48 +0200

    Background tranisition added

--- musicplayer-api/background.js
+++ musicplayer-api/background.js
@@ -3,11 +3,16 @@ var index = 0;
 
 
 body.addEventListener("mouseover", () =>{
+    body.style.opacity = '0';
+    console.log('opacity 0');
     setTimeout(() => {
         console.log(index);
         changeBackground(index);
         index = index+1;
+        body.style.opacity = '1';
+        console.log('opacity 1');
     }, 4000);
+    setTimeout(()=>{},10000);
 });
 
 function changeBackground(index){
