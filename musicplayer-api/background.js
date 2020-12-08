cb3c6464ba0442a6305867ec90b50c17b8482bb9
Iosif Tudor Andrei <tudor_iosif@yahoo.com>
2020-12-08 23:30:45 +0200

    Responsiveness added to navbar

--- musicplayer-api/background.js
+++ musicplayer-api/background.js
@@ -1,21 +1,21 @@
-const body = document.getElementById("body");
-var index = 0;
+// const body = document.getElementById("body");
+// var index = 0;
 
 
-body.addEventListener("mouseover", () =>{
-    body.style.opacity = '0';
-    console.log('opacity 0');
-    setTimeout(() => {
-        console.log(index);
-        changeBackground(index);
-        index = index+1;
-        body.style.opacity = '1';
-        console.log('opacity 1');
-    }, 4000);
-    setTimeout(()=>{},10000);
-});
+// body.addEventListener("mouseover", () =>{
+//     body.style.opacity = '0';
+//     console.log('opacity 0');
+//     setTimeout(() => {
+//         console.log(index);
+//         changeBackground(index);
+//         index = index+1;
+//         body.style.opacity = '1';
+//         console.log('opacity 1');
+//     }, 4000);
+//     setTimeout(()=>{},10000);
+// });
 
-function changeBackground(index){
-    index = index%4 + 1;
-    body.style.backgroundImage = `url(assets/bkg${index}.jpg)`;
-}
+// function changeBackground(index){
+//     index = index%4 + 1;
+//     body.style.backgroundImage = `url(assets/bkg${index}.jpg)`;
+// }
