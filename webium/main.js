05329655657c48db7531bf70de14ac7774ca0428
Iosif Tudor Andrei <tudor_iosif@yahoo.com>
2020-12-09 20:28:59 +0200

    full responsive navbar

--- webium/main.js
+++ webium/main.js
@@ -1,3 +1,26 @@
+const dots = document.getElementById("ham-menu");
+const links = document.getElementById("nav-links");
+const searchBox = document.getElementById("search-textbox");
+const searchIcon = document.getElementById("search-icon");
+
+searchIcon.addEventListener('click', () => { 
+  if(searchBox.classList.contains("show")){
+    searchBox.classList.remove("show");
+  }else{
+    searchBox.classList.add("show");
+  }
+});
+
+dots.addEventListener('click', () => {
+  if(dots.classList.contains("show")){
+    links.classList.add("show");
+    dots.classList.remove("show");
+  }else{
+    links.classList.remove("show");
+    dots.classList.add("show");
+  }
+});
+
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     speed: 1500,
