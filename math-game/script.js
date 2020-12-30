7ad451b3f869e61d8f0b18c62330170c1a8d08cf
Iosif Tudor Andrei <tudor_iosif@yahoo.com>
2020-12-30 22:51:30 +0200

    game scene transition

--- math-game/script.js
+++ math-game/script.js
@@ -2,16 +2,21 @@
 const playBtn = document.getElementById('play-btn');
 const menuBackground = document.getElementById('background-play');
 const gameBackground = document.getElementById('background-pink');
-
+const heroText = document.getElementById('hero-text');
+const btnContainer = document.getElementById('button-container');
 
 playBtn.addEventListener('click', () => {
 
     if(!menuBackground.classList.contains('hide')){
         changeClassList(menuBackground, 'hide', 'show');
         changeClassList(gameBackground, 'show', 'hide');
+        changeClassList(heroText,'hide','');
+        changeClassList(btnContainer,'hide','');
     }else{
         changeClassList(menuBackground, 'show', 'hide');
         changeClassList(gameBackground, 'hide', 'show');
+        changeClassList(heroText, '', 'hide');
+        changeClassList(btnContainer, '', 'hide');
     }
     restartAnimation(gameBackground, 1200);
     restartAnimation(menuBackground, 1200);
@@ -25,6 +30,8 @@ function restartAnimation(element, animationTime) {
 }
 
 function changeClassList(element, addClass, removeClass){
-    element.classList.remove(removeClass);
-    element.classList.add(addClass);
+    if(element.classList.contains(removeClass) && removeClass !=='')
+        element.classList.remove(removeClass);
+    if(!element.classList.contains(addClass) && addClass !== '')
+        element.classList.add(addClass);
 }
